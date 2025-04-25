eeb99feeeb5702be75dfdbce5ccb8d9caf369c8f
StijnHM <github-hb@stijn.hoefman.be>
2025-04-25 11:14:25 +0200

    Update Publish-Autopilot.ps1
    allow for multiple locations of TenantInfo files

--- Autopilot/Publish-Autopilot.ps1
+++ Autopilot/Publish-Autopilot.ps1
@@ -6,14 +6,22 @@ if ($psISE) {
 
 $ShortName = Read-Host "Client Short name"
 
-if (Test-Path "$ScriptRoot\TenantInfo_$($ShortName).7z") {
+$TenantInfoFiles = @()
+$TenantInfoFiles += Get-ChildItem "X:\OSDCloud\Config\Scripts\Autopilot\TenantInfo_*.7z"
+#$TenantInfoFiles += Get-ChildItem "$((Get-Volume | Where-Object { $_.FileSystemLabel -eq 'OSDCloudUSB' }).DriveLetter):\
+
+if ($TenantInfoFiles.Name -contains ("TenantInfo_$($Shortname).7z")) {
+    $TenantInfoFile = $TenantInfoFiles | Where-Object { $_.Name -eq "TenantInfo_$($Shortname).7z")
+}
+
+if (Test-Path "$($TenantInfoFile).FullName") {
     Write-Host "Sync DateTime"
     $DateTime = Invoke-RestMethod "https://postman-echo.com/time/now"
     $datetimeFormatted = [datetime]::Parse($datetime)
     Set-Date -Date $datetimeFormatted
 
     Write-Host "Extract encrypted 7z file"
-    $Arguments = 'x -aoa "{0}" -o"{1}" -p"{2}"' -f "$ScriptRoot\TenantInfo_$($ShortName).7z", "$ScriptRoot", (New-Object PSCredential 0, (Read-Host -Prompt "7z Encryption Password" -AsSecureString)).GetNetworkCredential().Password
+    $Arguments = 'x -aoa "{0}" -o"{1}" -p"{2}"' -f "$($TenantInfoFile).FullName", "$ScriptRoot", (New-Object PSCredential 0, (Read-Host -Prompt "7z Encryption Password" -AsSecureString)).GetNetworkCredential().Password
     $7za = Start-Process "$ScriptRoot\7z\7za.exe" -ArgumentList $Arguments -Wait -PassThru
     if ($7za.ExitCode -ne 0) {
         Write-Warning "7za exit code <> 0 ($($7za.ExitCode))"
