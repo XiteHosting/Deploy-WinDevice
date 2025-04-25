752099f85e1e1db6ee1526f1bd3c6a7ebd12ce82
StijnHM <github-hb@stijn.hoefman.be>
2025-04-25 10:19:27 +0200

    Update startup.ps1

--- startup.ps1
+++ startup.ps1
@@ -1,1 +1,28 @@
-# Startup file
+if ($psISE) {
+    $ScriptRoot = Split-Path -Path $psISE.CurrentFile.FullPath
+} else {
+    $ScriptRoot = $PSScriptRoot
+}
+
+$url = "https://github.com/XiteHosting/Deploy-WinDevice/archive/refs/heads/main.zip"
+$DownloadOutput = "$($env:TEMP)\Deploy-WinDevice.zip"
+$ArchiveOutput = "$($env:TEMP)\Deploy-WinDevice"
+
+
+Import-Module BitsTransfer
+Start-BitsTransfer -Source $url -Destination $DownloadOutput
+Expand-Archive -Path $DownloadOutput -DestinationPath $ArchiveOutput
+Remove-Item $DownloadOutput
+Move-Item "$ArchiveOutput\Deploy-WinDevice-main\*" $ArchiveOutput -Recursive
+Remove-Item "$ArchiveOutput\Deploy-WinDevice-main"
+
+Write-Host "Autopilot"
+Write-Host "Options: Publish Hash, Local CSV, Skip"
+$AutopilotOption = 'Publish'
+
+
+# Local CSV: To Do
+
+if ($AutopilotOption -eq 'Publish') {
+  "$ArchiveOutput\Autopilot\Publish-Autopilotinfo.ps1"
+}
