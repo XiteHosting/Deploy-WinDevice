c960de002d64e829de8b24276654150e470fc66c
StijnHM <github-hb@stijn.hoefman.be>
2025-04-25 09:51:18 +0200

    Add files via upload

---
+++ Autopilot/Create-7zArchive.ps1
@@ -0,0 +1,38 @@
+if ($psISE) {
+    Write-Host "ISE Running"
+    $ScriptRoot = Split-Path -Path $psISE.CurrentFile.FullPath        
+} else {
+    Write-Host "ISE Not Running"
+    $ScriptRoot = $PSScriptRoot
+}
+
+$ShortName = Read-Host "Client Short name"
+
+if (Test-Path "$ScriptRoot\TenantInfo_$($ShortName).7z") {
+    Write-Warning "File already exists!"
+} else {
+    Write-Host "Give Tenant App Configuration"
+    Write-Warning "No input checks, copy paste values !"
+    $TenantId = Read-Host -Prompt "Tenant Id"
+    $AppId = Read-Host -Prompt "App Id"
+    $AppSecret = Read-Host -Prompt "App Secret" -AsSecureString
+
+    Write-Host "Create encrypted 7z file"
+    $TenantInfo = @{TenantId = $TenantId; AppId = $AppId; AppSecret = (New-Object PSCredential 0, $AppSecret).GetNetworkCredential().Password}
+    $TenantInfo | ConvertTo-Json | Out-File "$ScriptRoot\TenantInfo.json"
+    $Arguments = 'a "{0}" "{1}" -sdel -p"{2}"' -f "$TenantInfo_$($ShortName).7z", "$ScriptRoot\TenantInfo.json", (New-Object PSCredential 0, (Read-Host -Prompt "7z Encryption Password" -AsSecureString)).GetNetworkCredential().Password
+    Start-Process "$ScriptRoot\7za.exe" -ArgumentList $Arguments -Wait
+
+    if (Test-Path "$ScriptRoot\TenantInfo.json") {
+        Write-Warning "Source file TenantInfo.json still exists, is the 7z created correctly?"
+    }
+
+    Write-Host "Testing encrypted 7z file"
+    $Arguments = 't "{0}" -p"{1}"' -f "$TenantInfo_$($ShortName).7z", (New-Object PSCredential 0, (Read-Host -Prompt "7z Encryption Password" -AsSecureString)).GetNetworkCredential().Password
+    $7za = Start-Process "$ScriptRoot\7za.exe" -ArgumentList $Arguments -Wait -PassThru
+    if ($7za.ExitCode -ne 0) {
+        Write-Warning "Couldn't open 7z file with specified password !"
+    } else {
+        Write-Host "Password ok"
+    }
+}
---
+++ Autopilot/Get-WindowsAutopilotInfoCsvWinPE.ps1
@@ -0,0 +1,42 @@
+if ($psISE) {
+    Write-Host "ISE Running"
+    $ScriptRoot = Split-Path -Path $psISE.CurrentFile.FullPath        
+} else {
+    Write-Host "ISE Not Running"
+    $ScriptRoot = $PSScriptRoot
+}
+
+$serial = (Get-WmiObject -Class Win32_BIOS).SerialNumber
+
+#Register PCPKsp
+Copy-Item "$ScriptRoot\PCPKsp.dll" "X:\Windows\System32\PCPKsp.dll"
+rundll32 X:\Windows\System32\PCPKsp.dll,DllInstall
+
+#Run OA3Tool
+Start-Process "$ScriptRoot\oa3tool.exe" -WorkingDirectory $ScriptRoot -ArgumentList "/Report /ConfigFile=""$ScriptRoot\OA3.cfg"" /NoKeyCheck" -Wait
+
+If (Test-Path $ScriptRoot\OA3.xml) 
+{
+
+	#Read Hash from generated XML File
+	[xml]$xmlhash = Get-Content -Path "$ScriptRoot\OA3.xml"
+	$hash=$xmlhash.Key.HardwareHash
+
+	#Delete XML File
+	Remove-Item $ScriptRoot\OA3.xml
+
+    #Create CSV File
+
+	$computers = @()
+	$product=""
+	# Create a pipeline object
+	$c = New-Object psobject -Property @{
+ 		"Device Serial Number" = $serial
+		"Windows Product ID" = $product
+		"Hardware Hash" = $hash
+		"Group Tag" = $GroupTag
+	}
+	
+ 	$computers += $c
+	$computers | Select "Device Serial Number", "Windows Product ID", "Hardware Hash", "Group Tag" | ConvertTo-CSV -NoTypeInformation | % {$_ -replace '"',''} | Out-File "$ScriptRoot\AutopilotInfo.csv"
+}
